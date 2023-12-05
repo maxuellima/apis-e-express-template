10167e92e3874ecfddcbd65c24b8f5ec9ee04cee
Maxuel Lima <maxuellima20@gmail.com>
2023-12-04 22:19:52 -0300

    Criando endpoitns com express

--- src/database.ts
+++ src/database.ts
@@ -1,4 +1,4 @@
-import { COURSE_STACK, TCourse } from "./types"
+import { COURSE_STACK, TCourse, TStudent } from "./types"
 
 export const courses: TCourse[] = [
     {
@@ -18,5 +18,43 @@ export const courses: TCourse[] = [
         name: "Express",
         lessons: 5,
         stack: COURSE_STACK.BACK
+    }
+]
+
+export const students: TStudent[] = [
+    {
+        id: "e001",
+        name: "Frodo Bolseiro",
+        age: 31
+    },
+    {
+        id: "e002",
+        name: "Samwise Gamgee",
+        age: 37
+    },
+    {
+        id: "003",
+        name: "Merry Brandybuck",
+        age: 36
+    },
+    {
+        id: "e004",
+        name: "Pippin Took",
+        age: 38
+    },
+    {
+        id:"e005",
+        name: "Aragorn II Elessar",
+        age: 39
+    },
+    {
+        id: "e006",
+        name: "Legolas Greenleaf",
+        age: 36
+    },
+    {
+        id: "e007",
+        name: "Gandalf the Grey",
+        age: 1001
     }
 ]
--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,7 @@
 import express, { Request, Response } from 'express'
 import cors from 'cors'
+import { courses, students } from './database'
+import { COURSE_STACK, TCourse, TStudent } from './types'
 
 const app = express()
 
@@ -12,4 +14,68 @@ app.listen(3003, () => {
 
 app.get('/ping', (req: Request, res: Response) => {
     res.send('Pong!')
+})
+
+//getAllCourses
+app.get("/courses", (req:Request, res:Response) =>{
+    res.send(courses)
+})
+
+//findCourseByName
+app.get("/courses/search",(req: Request, res: Response) =>{
+    const nameToFind = req.query.name as string
+    const result: TCourse[] = courses.filter((course)=>{
+        return course.name.toLowerCase().includes(nameToFind.toLowerCase())
+    })
+    res.send(result)
+})
+
+//createNewCourse
+app.post("/courses", (req: Request, res:Response)=>{
+    const id = req.body.id as string
+    const name = req.body.name as string
+    const lessons = req.body.lessons as number
+    const stack = req.body.stack as COURSE_STACK
+
+    const NewCourse = {
+        id,
+        name: name,
+        lessons,
+        stack
+
+    }
+
+    courses.push(NewCourse)
+    res.status(201).send("Curso registrado com sucesso!")
+    //No post normalmente só recebemos uma mensagem para saber se deu certo
+})
+
+
+//getAllStudents
+app.get("/students",(req: Request, res: Response)=>{
+    res.send(students)
+})
+
+//getStudentByName
+app.get("/students/search", (req: Request, res: Response)=>{
+    const name = req.query.name as string
+    const resultStudent : TStudent[] = students.filter((student)=>{
+        return student.name.toLowerCase().includes(name.toLowerCase())
+    })
+    res.send(resultStudent)
+})
+
+//postStudent
+app.post("/students", (req: Request, res: Response)=>{
+    const id = req.body.id as string
+    const name = req.body.name as string
+    const age = req.body.age as number
+
+    const newStudent = {
+        id,
+        name,
+        age
+    }
+    students.push(newStudent)
+    res.status(201).send("Aluno cadastrado com sucesso!")
 })
